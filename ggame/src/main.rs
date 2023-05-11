d0ed1edd38808cc4ca8caf8fdc46579cfee1182b
Adam V Stephen <adam.v.stephen@gmail.com>
2023-05-11 17:52:06 +0200

    Added the loop and the cast

--- ggame/src/main.rs
+++ ggame/src/main.rs
@@ -6,10 +6,31 @@ fn main() {
     println!("Initial exercise : guessing game");
     println!("What range ?");
 
-    let mut guess = String::new();
+    let pass_number = 314;
+    
 
+    loop {
+    let mut guess = String::new();
     io::stdin().read_line(&mut guess)
         .expect("Failed to read a line");
 
+    // Cast the shadow guess to integer type for comparison
+    //
+    // .expect seems unable to take a formatting construct
+    // .expect("expr {}", foo); is rejected
+    let guess :i32 = guess.trim().parse()
+        .expect("Looking for an integer");
     println!("You guessed {}", guess);
+    //
+// 
+
+
+    if (pass_number != guess) {
+        println!("Not the secret, guess again.");
+    } else {
+        println!("Congratulations {} ", pass_number);
+        return;
+    }
+
+    }
 }
