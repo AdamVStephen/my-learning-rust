29799e84b1b4115b402ed3ffcc7cc1578d929573
Adam V Stephen <adam.v.stephen@gmail.com>
2023-05-12 08:10:25 +0200

    Working on basic data types and function signatures.  Puzzle f32 -> String (str)

--- ggame/src/main.rs
+++ ggame/src/main.rs
@@ -1,9 +1,37 @@
 use std::io;
 use std::cmp::Ordering;
 
+// Variables and Types
+//
+const PI_INT :i32 = 314;
 
+fn show_pi() {
+    println!("Pi is approximately {} to an engineer", PI_INT/100);
+    let pi :f32 = PI_INT as f32;
+    let pi :f32 = (PI_INT as f32)/100.0;
+    println!("Pi is approximately {} to an accountant", PI_INT as f32/100.00);
+
+    // Build up some knowledge of function signatures
+    //
+
+    // Return the integer and fractional parts separately
+    let pi : (i32, f32) = calc_pi_i_f("hello");
+    let pi_frac : &str = String::from(pi.1)[2..];
+    println!("Pi is approximately {},{} computed by calc_pi", pi.0, pi_frac); 
+}
+
+//let pi_methods : (str, str) = ("hard_coded", "rational_approximation");
+
+fn calc_pi(method: &str) -> (f32, &str) {
+    (3.14159265, "hard_coded")
+}
+
+fn calc_pi_i_f(method: &str) -> (i32, f32) {
+    (3, 0.14159)
+}
 
 fn main() {
+    show_pi();
     println!("Initial exercise : guessing game");
     println!("What range ?");
 
